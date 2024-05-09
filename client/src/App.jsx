6445734f73243eb81c43b036a438c767d89f7c78
Kashyap Pandya <thecodezx@gmail.com>
2024-05-09 15:06:04 +0530

    [ADD]created store, redux presist, react rotuer setup, pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,25 @@
+// react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages import
+import HomePage from "./pages/homePage/HomePage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className='app'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<LoginPage />}></Route>
+					<Route path='/home' element={<HomePage />}></Route>
+					<Route
+						path='/profile/:userId'
+						element={<ProfilePage />}
+					></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,4 +2,48 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+//redux toolkit
+import authReducer from "./app/store.js";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+//redux presist
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					FLUSH,
+					REHYDRATE,
+					PAUSE,
+					PERSIST,
+					PURGE,
+					REGISTER,
+				],
+			},
+		}),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistStore(store)}>
+			<App />
+		</PersistGate>
+	</Provider>
+);
